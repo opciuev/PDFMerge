8c48b6e83db10766c59e8360933c1193416f6f9b
unknown <shepawykiho1@outlook.com>
2023-08-13 21:44:40 +0800

    add processbar

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from PIL import Image
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import configparser
+import threading
+import tkinter as tk
+from tkinter import ttk  # 导入ttk模块
 
 class ImageToPdfConverter:
     def __init__(self, root):
@@ -23,11 +26,18 @@ class ImageToPdfConverter:
         self.convert_button = tk.Button(self.root, text="Convert to PDF", command=self.convert_to_pdf)
         self.convert_button.pack()
 
+        self.progress_label = tk.Label(self.root, text="", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.progress_label.pack(fill=tk.X)
+
+        self.progress_bar = ttk.Progressbar(self.root, length=300, mode="determinate")
+        self.progress_bar.pack()
+        
+
     def load_config(self):
         self.config = configparser.ConfigParser()
         if not os.path.exists("config.ini"):
-            self.config.add_section("Paths")  # 添加默认的'Paths'部分
-            self.save_config()  # 保存配置文件
+            self.config.add_section("Paths")
+            self.save_config()
         self.config.read("config.ini")
         self.image_folder = self.config.get("Paths", "ImageFolder", fallback="")
         self.output_folder = self.config.get("Paths", "OutputFolder", fallback="")
@@ -55,7 +65,7 @@ class ImageToPdfConverter:
 
         c = canvas.Canvas(pdf_filename, pagesize=letter)
 
-        for image_file in image_files:
+        for idx, image_file in enumerate(image_files):
             image_path = os.path.join(self.image_folder, image_file)
             img = Image.open(image_path)
             img_width, img_height = img.size
@@ -64,21 +74,39 @@ class ImageToPdfConverter:
             c.drawImage(image_path, 0, 0, width=img_width, height=img_height)
             c.showPage()
 
+            # 更新进度信息和进度条
+            progress_percent = (idx + 1) / len(image_files) * 100
+            self.progress_label.config(text=f"Processing: {idx + 1}/{len(image_files)}")
+            self.progress_bar["value"] = progress_percent
+            self.progress_bar.update()
+
         c.save()
 
     def convert_to_pdf(self):
         if not self.image_folder or not self.output_folder:
             return
 
-        self.merge_images_to_pdf()
-        messagebox.showinfo("Conversion Complete", "Images have been converted to PDF.")
+        # 清空进度信息和进度条
+        self.progress_label.config(text="")
+        self.progress_bar["value"] = 0
 
+        # 在新线程中执行文件操作
+        thread = threading.Thread(target=self.execute_file_operation)
+        thread.start()
+
+    def execute_file_operation(self):
+        self.merge_images_to_pdf()
+
+        # 更新进度信息和进度条
+        self.progress_label.config(text="Conversion completed!")
+        self.progress_bar["value"] = 100
+
+        # 自动打开 PDF 文件
         pdf_filename = os.path.join(self.output_folder, f"{os.path.basename(self.image_folder)}.pdf")
-    
         try:
-            subprocess.Popen(["start", "", pdf_filename], shell=True)  # 在 Windows 上打开 PDF 文件
+            subprocess.Popen(["start", "", pdf_filename], shell=True)
         except:
-            pass  # 如果无法打开，则忽略错误
+            pass
 
 def main():
     root = tk.Tk()
