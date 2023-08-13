49cd4a8c9901580517efa8cbadd7fa5b04289364
unknown <shepawykiho1@outlook.com>
2023-08-13 20:59:03 +0800

    Fix PDF Page Size

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# 我希望使用Python实现一个图片合并为PDF文件的小程序
-# 1. 指定一个路径，路径下有很多文件夹，每个文件夹下有很多图片
-# 2. 将每个文件夹下的图片合并为一个PDF文件，PDF文件名为文件夹名
-# 3. 需要书签，书签就是图片名
-# 举个例子
-# 1. 指定路径为C:\macshare\DLRAW.NET-Tsugumomo vol 01-30\DLRAW.NET-Tsugumomo v01-02
-# 2. C:\macshare\DLRAW.NET-Tsugumomo vol 01-30\DLRAW.NET-Tsugumomo v01-02下有文件夹“DLRAW.NET-[浜田よしかづ] つぐもも 第01巻”，“DLRAW.NET-[浜田よしかづ] つぐもも 第02巻”
-# 3. “DLRAW.NET-[浜田よしかづ] つぐもも 第01巻”下有图片“DLRAW.NET-0001.jpg”，“DLRAW.NET-0002.jpg”
-# 下面开始写代码
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from PIL import Image
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -51,6 +42,7 @@ class ImageToPdfConverter:
             img = Image.open(image_path)
             img_width, img_height = img.size
 
+            c.setPageSize((img_width, img_height))
             c.drawImage(image_path, 0, 0, width=img_width, height=img_height)
             c.showPage()
 
@@ -61,7 +53,7 @@ class ImageToPdfConverter:
             return
 
         self.merge_images_to_pdf()
-        tk.messagebox.showinfo("Conversion Complete", "Images have been converted to PDF.")
+        messagebox.showinfo("Conversion Complete", "Images have been converted to PDF.")
 
 def main():
     root = tk.Tk()
